d6ce4c25e7d5d9a2aeab3a25e5a58d2e219cd251
Victor <victoralissont5@gmail.com>
2017-08-09 15:47:54 -0300

    Create FireRecord project via pod create lib

---
+++ Example/Pods/Target Support Files/FireRecord/FireRecord-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FireRecord : NSObject
+@end
+@implementation PodsDummy_FireRecord
+@end
---
+++ Example/Pods/Target Support Files/Pods-FireRecord_Example/Pods-FireRecord_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FireRecord_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FireRecord_Example
+@end
