c52837a4917909aa47e2cd10c0d50dea2ce4dccc
David Sanford <dsanfordseu@gmail.com>
2017-08-15 02:01:21 -0300

    Initialize firebase through fireRecord configuration

--- Example/Pods/Target Support Files/Pods-FireRecord_Example/Pods-FireRecord_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-FireRecord_Example/Pods-FireRecord_Example-frameworks.sh
@@ -90,9 +90,17 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/FireRecord/FireRecord.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FirebaseCommunity/FirebaseCommunity.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/leveldb-library/leveldb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/FireRecord/FireRecord.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FirebaseCommunity/FirebaseCommunity.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/leveldb-library/leveldb.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
