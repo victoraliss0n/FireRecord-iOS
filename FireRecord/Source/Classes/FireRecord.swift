efc27f9457ecf8fc6e256245ec80c43906788d5e
Victor <victoralissont5@gmail.com>
2017-08-24 20:36:17 -0300

    Update FireRecord to not require init with decoder

--- FireRecord/Source/Classes/FireRecord.swift
+++ FireRecord/Source/Classes/FireRecord.swift
@@ -3,7 +3,12 @@ import FirebaseCommunity
 open class FireRecord: FirebaseModel {
     var key: String?
     
+    required public init() {}
+    
     public static func configure() {
         FirebaseApp.configure()
     }
 }
+
+
+
--- FireRecord/Source/Protocols/Database/FirebaseModel.swift
+++ FireRecord/Source/Protocols/Database/FirebaseModel.swift
@@ -9,5 +9,6 @@ import FirebaseCommunity
 
 protocol FirebaseModel: ModelType where Reference == DatabaseReference  {
     var key: String? { get set }
+    init()
     func toJSON() -> [String: Any]
 }
