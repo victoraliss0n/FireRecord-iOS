d9e3455769350647bfa39fc27c95b37e98dfefa7
Victor <victoralissont5@gmail.com>
2017-08-29 20:28:24 -0300

    Update reading protocol with new way of deserializing

--- FireRecord/Source/Extensions/Database/Readable+Extension.swift
+++ FireRecord/Source/Extensions/Database/Readable+Extension.swift
@@ -6,11 +6,21 @@
 //
 
 import Foundation
+import FirebaseCommunity
 
 public extension Readable where Self: FirebaseModel {
     static func all(completion: @escaping (_ object: [Self]
         ) -> Void)  {
-       
+        Self.classPath.observe(.value) { snapshot in
+            let dataSnapshot = snapshot.children.allObjects as? [DataSnapshot]
+            let arraySelf = dataSnapshot?.flatMap({Self.deserialize(from: $0.value as? NSDictionary)})
+            if let objects = arraySelf {
+                completion(objects)
+            }
+        }
+    }
+    static func limit(value: Int8) -> Self.Type {
+        return self
     }
 
 }
