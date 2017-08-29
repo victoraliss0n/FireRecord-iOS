68f518920fa69b5ae475f56578875929829232da
Victor <victoralissont5@gmail.com>
2017-08-29 19:32:57 -0300

    Update FirebaseModel with new serialization method

--- FireRecord/Source/Extensions/Database/FirebaseModel+Extension.swift
+++ FireRecord/Source/Extensions/Database/FirebaseModel+Extension.swift
@@ -11,25 +11,19 @@ import FirebaseCommunity
 public extension FirebaseModel {
     typealias JSON = [String: Any]
     
-    static var path: DatabaseReference {
+    internal static var classPath: DatabaseReference {
         return reference.child(Self.className)
     }
     static var reference: DatabaseReference {
         return Database.database().reference()
     }
-    static var className: String {
+    internal static var className: String {
         return String(describing: self)
     }
-    static var autoId: String {
+    internal static var autoId: String {
         return Self.reference.childByAutoId().key
     }
     func toJSON() -> JSON {
-        guard let jsonData = try? JSONEncoder().encode(self) else {
-            return JSON()
-        }
-        guard let dictionary = try? JSONSerialization.jsonObject(with: jsonData, options: []) as? JSON ?? JSON() else {
-            return JSON()
-        }
-        return dictionary
+        return toJSON()
     }
 }
