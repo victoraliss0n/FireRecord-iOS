ee2f4355a185846f318578fc826e2bc0a38d31d6
Victor <victoralissont5@gmail.com>
2017-08-29 18:09:55 -0300

    Add structure whose responsibility is to isolate the configuration of the FireRecord module

---
+++ FireRecord/Source/FireRecordApp.swift
@@ -0,0 +1,15 @@
+//
+//  FireRecordApp.swift
+//  FireRecord
+//
+//  Created by Victor Alisson on 29/08/17.
+//
+
+import Foundation
+import FirebaseCommunity
+
+public struct FireRecordApp {
+    public static func configure() {
+        FirebaseApp.configure()
+    }
+}
