b36c9aa8d2d3386ed2237eccf51235de4068c8a8
Victor <victoralissont5@gmail.com>
2017-08-30 00:38:38 -0300

    Add default implementation of the Filterable protocol

--- FireRecord/Source/Extensions/Database/Filterable+Extension.swift
+++ FireRecord/Source/Extensions/Database/Filterable+Extension.swift
@@ -6,10 +6,18 @@
 //
 
 import Foundation
+import FirebaseCommunity
 
 public extension Filterable where Self: FirebaseModel  {
     static func findFirst(completion: @escaping (_ object: Self) -> Void)  {
-        
+        Self.classPath.queryLimited(toLast: 1).observeSingleEvent(of: .value) { snapshot in
+            let dataSnapshot = snapshot.children.allObjects.first as? DataSnapshot
+            let object = dataSnapshot.flatMap({Self.deserialize(from: $0.value as? NSDictionary)})
+            object?.key = dataSnapshot?.key
+            if let firebaseModel = object {
+                completion(firebaseModel)
+            }
+        }
     }
 }
 
