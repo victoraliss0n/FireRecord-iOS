0187cb9bf8a9b47914f0d89f5c568513ae09c069
Victor <victoralissont5@gmail.com>
2017-09-28 15:09:40 -0300

    Redefine joining of protocols that define Active Record

--- FireRecord/Source/Protocols/Database/ActiveRecord.swift
+++ FireRecord/Source/Protocols/Database/ActiveRecord.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public protocol ActiveRecord: Writeable, Deletable, Readable, Updatable, Storator {
+public protocol ActiveRecord: Writeable, Deletable, Readable, ReadableInRealTime, Updatable, Storator {
 
 }
 
