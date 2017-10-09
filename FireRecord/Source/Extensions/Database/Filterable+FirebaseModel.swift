99276f2edab89ed969c6cf0f9ee6a5f81b719b5e
Victor <victoralissont5@gmail.com>
2017-10-09 02:12:55 -0300

    Update queries from purely functional operations

--- FireRecord/Source/Extensions/Database/Filterable+FirebaseModel.swift
+++ FireRecord/Source/Extensions/Database/Filterable+FirebaseModel.swift
@@ -11,22 +11,29 @@ import FirebaseCommunity
 public extension Filterable where Self: FirebaseModel  {
     static func findFirst(completion: @escaping (_ object: Self) -> Void)  {
         Self.classPath.queryLimited(toFirst: 1).observeSingleEvent(of: .value) { snapshot in
-            if let firebaseModel = Self.getFirebaseModels(snapshot)?.first{completion(firebaseModel)}
+            if let firebaseModel = Self.getFirebaseModels(snapshot).first {
+                completion(firebaseModel)
+            }
         }
     }
     static func findLast(completion: @escaping (_ object: Self) -> Void) {
         Self.classPath.queryLimited(toLast: 1).observeSingleEvent(of: .value) { snapshot in
-            if let firebaseModel = Self.getFirebaseModels(snapshot)?.first {completion(firebaseModel)}
+            if let firebaseModel = Self.getFirebaseModels(snapshot).first {
+                completion(firebaseModel)
+            }
         }
     }
     static func findFirst(_ toFirst: UInt, completion: @escaping (_ object: [Self]) -> Void) {
+        
         Self.classPath.queryLimited(toFirst: toFirst).observeSingleEvent(of: .value) { snapshot in
-            if let firebaseModel = Self.getFirebaseModels(snapshot) {completion(firebaseModel)}
+             let firebaseModel = Self.getFirebaseModels(snapshot)
+             completion(firebaseModel)
         }
     }
     static func findLast(_ toLast: UInt, completion: @escaping (_ object: [Self]) -> Void) {
         Self.classPath.queryLimited(toLast: toLast).observeSingleEvent(of: .value) { snapshot in
-            if let firebaseModels = Self.getFirebaseModels(snapshot) {completion(firebaseModels)}
+            let firebaseModels = Self.getFirebaseModels(snapshot)
+            completion(firebaseModels)
         }
     }
    
--- FireRecord/Source/Extensions/Database/RealTime/FilterableInRealTime+FirebaseModel.swift
+++ FireRecord/Source/Extensions/Database/RealTime/FilterableInRealTime+FirebaseModel.swift
@@ -10,22 +10,24 @@ import Foundation
 public extension FilterableInRealTime where Self: FirebaseModel {
     static func observeFindFirst(completion: @escaping (_ object: Self) -> Void)  {
         Self.classPath.queryLimited(toFirst: 1).observe(.value) { snapshot in
-            if let firebaseModel = Self.getFirebaseModels(snapshot)?.first{completion(firebaseModel)}
+             if let firebaseModel = Self.getFirebaseModels(snapshot).first { completion(firebaseModel) }
         }
     }
     static func observeFindLast(completion: @escaping (_ object: Self) -> Void) {
         Self.classPath.queryLimited(toLast: 1).observe(.value) { snapshot in
-            if let firebaseModel = Self.getFirebaseModels(snapshot)?.first {completion(firebaseModel)}
+            if let firebaseModel = Self.getFirebaseModels(snapshot).first { completion(firebaseModel) }
         }
     }
     static func observeFindFirst(_ toFirst: UInt, completion: @escaping (_ object: [Self]) -> Void) {
         Self.classPath.queryLimited(toFirst: toFirst).observe(.value) { snapshot in
-            if let firebaseModel = Self.getFirebaseModels(snapshot) {completion(firebaseModel)}
+            let firebaseModel = Self.getFirebaseModels(snapshot)
+            completion(firebaseModel)
         }
     }
     static func observeFindLast(_ toLast: UInt, completion: @escaping (_ object: [Self]) -> Void) {
         Self.classPath.queryLimited(toLast: toLast).observe(.value) { snapshot in
-            if let firebaseModels = Self.getFirebaseModels(snapshot) {completion(firebaseModels)}
+             let firebaseModels = Self.getFirebaseModels(snapshot)
+             completion(firebaseModels)
         }
     }
 }
