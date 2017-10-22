9fdc424e3a59d2c46f5fb4980a2947a01fff2d76
Victor <victoralissont5@gmail.com>
2017-10-22 15:18:37 -0300

    Add abstraction that joins all protocols that work in real time

---
+++ FireRecord/Source/Protocols/Database/RealTime/RealTime.swift
@@ -0,0 +1,12 @@
+//
+//  RealTime.swift
+//  FireRecord
+//
+//  Created by Victor Alisson on 22/10/17.
+//
+
+import Foundation
+
+public protocol RealTime: ReadableInRealTime, RemovableObserver {
+    
+}
