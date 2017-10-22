ec50bd9c28ec56991606d3073747af8eb145573d
Victor <victoralissont5@gmail.com>
2017-10-22 16:37:42 -0300

    Add listener properties to events in the Readable

--- FireRecord/Source/Extensions/Database/Readable+FirebaseModel.swift
+++ FireRecord/Source/Extensions/Database/Readable+FirebaseModel.swift
@@ -10,30 +10,30 @@ import FirebaseCommunity
 
 public extension Readable where Self: FirebaseModel  {
     
-    static func all(completion: @escaping (_ object: [Self]
+    static func all(when propertyEventType: PropertyEventType, completion: @escaping (_ object: [Self]
         ) -> Void)  {
-        Self.classPath.observeSingleEvent(of: .value) { snapshot in
+        Self.classPath.observeSingleEvent(of: propertyEventType.rawValue) { snapshot in
             let firebaseModels = Self.getFirebaseModels(snapshot)
             completion(firebaseModels)
         }
     }
-    static func find(completion: @escaping (_ objects: [Self]) -> Void) {
-        Self.fireRecordQuery?.observeSingleEvent(of: .value) { snapshot in
+    static func find(when propertyEventType: PropertyEventType, completion: @escaping (_ objects: [Self]) -> Void) {
+        Self.fireRecordQuery?.observeSingleEvent(of: propertyEventType.rawValue) { snapshot in
             let firebaseModels = Self.getFirebaseModels(snapshot)
             completion(firebaseModels)
         }
     }
     
-    static func find(byKey key: String, completion: @escaping (_ objects: Self) -> Void) {
-        Self.classPath.child(key).observeSingleEvent(of: .value) { snapshot in
+    static func find(when propertyEventType: PropertyEventType, byKey key: String, completion: @escaping (_ objects: Self) -> Void) {
+        Self.classPath.child(key).observeSingleEvent(of: propertyEventType.rawValue) { snapshot in
             if let model = Self.getFirebaseModel(from: snapshot) {
                 completion(model)
             }
         }
     }
-     func findByKey(completion: @escaping (_ object: Self) -> Void) {
+     func findByKey(when propertyEventType: PropertyEventType, completion: @escaping (_ object: Self) -> Void) {
         guard let key = self.key else { return }
-        Self.classPath.child(key).observeSingleEvent(of: .value) { snapshot in
+        Self.classPath.child(key).observeSingleEvent(of: propertyEventType.rawValue) { snapshot in
             if let model = Self.getFirebaseModel(from: snapshot) {
                 completion(model)
             }
