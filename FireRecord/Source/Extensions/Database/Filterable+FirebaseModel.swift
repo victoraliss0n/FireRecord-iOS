571327e047d253a653c55b52d76054483fd24604
Victor <victoralissont5@gmail.com>
2017-10-25 21:53:17 -0300

    Fix: The implementation now with all methods using FireRecordQuery correctly

--- FireRecord/Source/Extensions/Database/Filterable+FirebaseModel.swift
+++ FireRecord/Source/Extensions/Database/Filterable+FirebaseModel.swift
@@ -10,14 +10,14 @@ import FirebaseCommunity
 
 public extension Filterable where Self: FirebaseModel  {
     static func findFirst(when propertyEventType: PropertyEventType, completion: @escaping (_ object: Self) -> Void)  {
-        Self.classPath.queryLimited(toFirst: 1).observeSingleEvent(of: propertyEventType.rawValue) { snapshot in
+        Self.fireRecordQuery?.queryLimited(toFirst: 1).observeSingleEvent(of: propertyEventType.rawValue) { snapshot in
             if let firebaseModel = Self.getFirebaseModels(snapshot).first {
                 completion(firebaseModel)
             }
         }
     }
     static func findLast(when propertyEventType: PropertyEventType, completion: @escaping (_ object: Self) -> Void) {
-        Self.classPath.queryLimited(toLast: 1).observeSingleEvent(of: propertyEventType.rawValue) { snapshot in
+       Self.fireRecordQuery?.queryLimited(toLast: 1).observeSingleEvent(of: propertyEventType.rawValue) { snapshot in
             if let firebaseModel = Self.getFirebaseModels(snapshot).first {
                 completion(firebaseModel)
             }
@@ -25,13 +25,13 @@ public extension Filterable where Self: FirebaseModel  {
     }
     static func findFirst(when propertyEventType: PropertyEventType, _ toFirst: UInt, completion: @escaping (_ object: [Self]) -> Void) {
         
-        Self.classPath.queryLimited(toFirst: toFirst).observeSingleEvent(of: propertyEventType.rawValue) { snapshot in
+        Self.fireRecordQuery?.queryLimited(toFirst: toFirst).observeSingleEvent(of: propertyEventType.rawValue) { snapshot in
              let firebaseModel = Self.getFirebaseModels(snapshot)
              completion(firebaseModel)
         }
     }
     static func findLast(when propertyEventType: PropertyEventType, _ toLast: UInt, completion: @escaping (_ object: [Self]) -> Void) {
-        Self.classPath.queryLimited(toLast: toLast).observeSingleEvent(of: propertyEventType.rawValue) { snapshot in
+        Self.fireRecordQuery?.queryLimited(toLast: toLast).observeSingleEvent(of: propertyEventType.rawValue) { snapshot in
             let firebaseModels = Self.getFirebaseModels(snapshot)
             completion(firebaseModels)
         }
