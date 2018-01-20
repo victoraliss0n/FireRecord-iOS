3305e838f35a42cd804328c24dbe68f5de2ac311
David Sanford <dsanfordseu@gmail.com>
2018-01-20 16:32:41 -0300

    Add progress method to uploadable protocol

--- FireRecord/Source/Protocols/Storage/Uploadable.swift
+++ FireRecord/Source/Protocols/Storage/Uploadable.swift
@@ -7,10 +7,13 @@
 
 import Foundation
 
+public typealias ProgressObserver = (_ progress: Double) -> Void
+
 public protocol Uploadable {
     
     var path: String? { get set }
     var data: Data? { get }
     
-    func upload(fileName: String, path: String)
+    func upload(fileName: String, path: String, onProgress: ProgressObserver?)
+    func observeUploadProgress(onProgress: @escaping (_ progress: Double) -> Void)
 }
