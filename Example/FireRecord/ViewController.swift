1115d9f5cf5a1fce8b5ef660d1fb419950ca0f2d
David Sanford <dsanfordseu@gmail.com>
2018-01-20 23:32:00 -0300

    Add upload progress example call to view controller

--- Example/FireRecord/ViewController.swift
+++ Example/FireRecord/ViewController.swift
@@ -18,9 +18,15 @@ class ViewController: UIViewController {
         
         user.name = "Victor"
         user.photo = FirebaseImage(#imageLiteral(resourceName: "Image"))
+        
+        user.photo?.observeUploadProgress { progress in
+            print("\(progress)% uploaded")
+        }
+        
         user.save { error in
             print(error?.localizedDescription ?? "No type of error")
         }
+        
         User.findFirst(when: .anyChange) { user in
             user.name = "Mangueira"
             user.update(completion: { error in
